c6acc15620b70d4a3eec0c0ddedddd5a1111cc80
SchwarzRene <20191028@atn.ac.at>
2025-03-24 21:08:33 +0100

    drawing

--- ball-bouncing-simulation/src/main.rs
+++ ball-bouncing-simulation/src/main.rs
@@ -1,10 +1,28 @@
+use macroquad::prelude::*;
+
+enum RetschisColour{
+    Red,
+    Green,
+    Blue
+}
+
+enum Size{
+    S,
+    M,
+    L
+}
+
+
 struct Ball{
     pos : Vec<f64>,
     v : Vec<f64>,
-    a : Vec<f64>
+    a : Vec<f64>,
+    color : RetschisColour,
+    size : Size
 }
 
-fn main() {
+#[macroquad::main("Ball Simulation")]
+async fn main() {
     let position = vec![ 0., 0. ];
     let velocity = vec![ 0., 0. ];
     let acceleration = vec![ 0., 0. ];
@@ -12,9 +30,31 @@ fn main() {
     let b = Ball{
         pos : position,
         v : velocity,
-        a : acceleration
+        a : acceleration,
+        color : RetschisColour::Red,
+        size : Size::S
     };
-    
 
+    loop {
+        clear_background( BLACK );
 
+        draw_circle( 0., 0., size_convertion( &b.size ), get_color( &b.color ) );
+        next_frame().await;
+    };
+}
+
+fn size_convertion( size : &Size ) -> f32{
+    match size{
+        Size::S => 100.,
+        Size::M => 200.,
+        Size::L => 300.
+    }
+}
+
+fn get_color( c : &RetschisColour ) -> Color{
+    match c{
+        RetschisColour::Red => RED,
+        RetschisColour::Green => GREEN,
+        RetschisColour::Blue => BLUE
+    }
 }
